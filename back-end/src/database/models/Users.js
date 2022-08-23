d66a05ec12c3efcc155246c2a7892a095a28e9e7
C.Swame <caio_swame@hotmail.com>
2022-08-23 18:56:20 -0300

    chore[backend database models]: creates a model 'Users' to handle the table 'users'

---
+++ back-end/src/database/models/Users.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Users.hasMany(models.Sales, { foreignKey: 'user_id', as: 'sales' });
+      Users.hasMany(models.Sales, { foreignKey: 'seller_id', as: 'sales' });
+    }
+  }
+  Users.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'Users',
+    timestamps: false,
+    tableName: 'users'
+  });
+  return Users;
+};
