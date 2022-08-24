be46423b2040a85d8b2cb827e63b45c9f8a8b2e4
André <amorimtorresandre@gmail.com>
2022-08-24 17:40:44 -0300

    FEAT: setting Login page form with inputs and buttons

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
-      <h1> LOGIN PAGE</h1>
+      <form>
+        <label htmlFor="email">
+          <input
+            type="text"
+            aria-label="email"
+            onChange={ ({ target }) => setEmail(target.value) }
+            value={ email }
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="text"
+            aria-label="password"
+            onChange={ ({ target }) => setPassword(target.value) }
+            value={ password }
+          />
+        </label>
+        <button
+          type="submit"
+          aria-label="login-button"
+          // onClick={}
+        >
+          LOGIN
+        </button>
+        <button
+          type="submit"
+          aria-label="signup-button"
+          // onClick={}
+        >
+          Ainda não tenho conta
+        </button>
+      </form>
     </div>
   );
 }
