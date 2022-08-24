36cbfcbd427c2ab2bdc85eebe179fc3a953f1274
André <amorimtorresandre@gmail.com>
2022-08-24 18:32:28 -0300

    SET: inittial setting Register page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
 import Login from './Pages/Login';
+import Register from './Pages/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={ <Login /> } />
+        <Route path="/register" element={ <Register /> } />
         <Route exact path="/" element={ <Navigate to="/login" /> } />
       </Routes>
     </BrowserRouter>
