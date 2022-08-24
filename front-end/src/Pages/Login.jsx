d4c352a94e5f9fb0ceab3e47af55a09638601af1
André <amorimtorresandre@gmail.com>
2022-08-24 18:23:54 -0300

    FEAT: handleClick logic button and redirect Link to /register on register-button

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,8 +1,28 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { changeEmail } from '../Redux/slicers/user.slicer';
 
 function Login() {
+  const dispatch = useDispatch();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const isButtonDisabled = () => {
+    const PASSWORD_LENGTH = 6;
+    const regex = /^\S+@\S+\.\S+$/;
+    return (
+      email !== ''
+        && regex.test(email)
+        && password.length >= PASSWORD_LENGTH
+    );
+  };
+
+  const onClickButton = (event) => {
+    event.preventDefault();
+    dispatch(changeEmail(email));
+  };
 
   return (
     <div>
@@ -13,30 +33,38 @@ function Login() {
             aria-label="email"
             onChange={ ({ target }) => setEmail(target.value) }
             value={ email }
+            data-testid="common_login__input-email"
           />
         </label>
         <label htmlFor="password">
           <input
-            type="text"
+            type="password"
             aria-label="password"
             onChange={ ({ target }) => setPassword(target.value) }
             value={ password }
+            data-testid="common_login__input-password"
           />
         </label>
         <button
           type="submit"
           aria-label="login-button"
-          // onClick={}
+          disabled={ !isButtonDisabled() }
+          onClick={ onClickButton }
+          data-testid="common_login__button-login"
+          name="login-button"
         >
           LOGIN
         </button>
-        <button
-          type="submit"
-          aria-label="signup-button"
-          // onClick={}
-        >
-          Ainda não tenho conta
-        </button>
+        <Link to="/register">
+          <button
+            type="button"
+            aria-label="signup-button"
+            data-testid="common_login__button-register"
+            name="signup-button"
+          >
+            Ainda não tenho conta
+          </button>
+        </Link>
       </form>
     </div>
   );
