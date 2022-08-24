a34de8786ebc0131f4ec40d304d311a9af3dfa15
C.Swame <caio_swame@hotmail.com>
2022-08-24 12:24:46 -0300

    chore[backend auth]: creates a file 'jwt.js' auth helper.
    methods:
       generateToken: recevis a payload and returns a token
       verifyAndReadToken: recives a token and returns it value decrypted

--- back-end/src/auth/jwt.js
+++ back-end/src/auth/jwt.js
@@ -1,1 +1,30 @@
-// a implementar
+import * as jwt from '../../utils/jwt';
+import doetenv from ('dotenv');
+
+doetenv.config();
+
+const jwtConfig = {
+  expiresIn: '30m', // nome das chaves não deve ser alterado;
+  algorithm: 'HS256',
+};
+
+const jwtSecret = process.env.JWT_SECRET || 'Swordfish'
+
+export const generateToken = (payload, config = jwtConfig) => {
+  const { codCliente, admin } = payload;
+  return jwt.sign(
+    { codCliente, admin },
+    jwtSecret,
+    config,
+  );
+};
+
+export const verifyAndReadToken = (token) => {
+  return jwt.verify(token, jwtSecret)
+};
+
+export default {
+  generateToken,
+  verifyAndReadToken,
+}
+
