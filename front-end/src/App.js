7364d53b85862cb7978e03eebe1bae219da11812
André <amorimtorresandre@gmail.com>
2022-08-24 17:26:50 -0300

    FEAT: starting Login page and login route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+
+import Login from './Pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">GANGUE GANGRENA</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={ <Login /> } />
+        <Route exact path="/" element={ <Navigate to="/login" /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
