00750bd2750e9df10cbf01980e4ca926f4f45ead
6u7o <augustopinheiro19999999@gmail.com>
2022-08-24 19:39:03 -0300

    DOC [backend]: middleware error example

--- back-end/src/middlewares/errors/errorMiddleware.middlewares.js
+++ back-end/src/middlewares/errors/errorMiddleware.middlewares.js
@@ -1,1 +1,6 @@
-// a implementar
+const errorMiddleware = (error, req, res, next) => {
+  console.log(error.message);  
+  res.status(500).json({message: error.message, erro: 'teste'});
+};
+
+module.exports = errorMiddleware;
