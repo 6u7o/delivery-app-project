3c7a53f7c7f1c1429bd0a4a06c0f03a42f915469
C.Swame <caio_swame@hotmail.com>
2022-08-24 12:39:36 -0300

    lint[auth jwt]: fix some lint major erros

--- back-end/src/auth/jwt.js
+++ back-end/src/auth/jwt.js
@@ -1,30 +1,24 @@
-import * as jwt from '../../utils/jwt';
-import doetenv from ('dotenv');
+import * as jwt from 'jsonwebtoken';
 
-doetenv.config();
+require('dotenv').config();
 
 const jwtConfig = {
   expiresIn: '30m', // nome das chaves não deve ser alterado;
   algorithm: 'HS256',
 };
 
-const jwtSecret = process.env.JWT_SECRET || 'Swordfish'
+const jwtSecret = process.env.JWT_SECRET || 'Swordfish';
 
 export const generateToken = (payload, config = jwtConfig) => {
   const { codCliente, admin } = payload;
-  return jwt.sign(
-    { codCliente, admin },
-    jwtSecret,
-    config,
-  );
+  return jwt.sign({ codCliente, admin }, jwtSecret, config);
 };
 
-export const verifyAndReadToken = (token) => {
-  return jwt.verify(token, jwtSecret)
-};
+export const verifyAndReadToken = (token) => (
+  jwt.verify(token, jwtSecret)
+);
 
 export default {
   generateToken,
   verifyAndReadToken,
-}
-
+};
