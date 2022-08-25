1be781da50aa51456deaae1d69e0cc0812ef8897
André <amorimtorresandre@gmail.com>
2022-08-25 18:11:06 -0300

    FEAT: saving token, userName, userEmail and role on local storage

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 // import axios from 'axios';
 import { useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate /* useEffect */ } from 'react-router-dom';
 import api from '../services/request';
 import { changeEmail } from '../Redux/slicers/user.slicer';
 
@@ -23,13 +23,24 @@ function Login() {
     );
   };
 
+  /*   useEffect(() => {
+    const token = localStorage.getItem('token');
+    const role = localStorage.getItem('role');
+    const name = localStorage.getItem('userName');
+    const email =
+  }, []); */
+
   const onClickButton = async (event) => {
     event.preventDefault();
     try {
       const loginData = await api.post('/login', { email, password });
       if (loginData.error) return setError(true);
+      console.log(loginData);
       dispatch(changeEmail(email));
-      // *** salvar o token recebido; // loginData.data.token
+      localStorage.setItem('role', loginData.data.role);
+      localStorage.setItem('token', loginData.data.token);
+      localStorage.setItem('userEmail', loginData.data.email);
+      localStorage.setItem('userName', loginData.data.name);
       // *** tratar o erro de forma diferente. Quando os requests tem retorno com status de erro o programa está crashando. Buscar uma opção do axios ou react que evite isso (se existir);
       // *** Apesar do problema acima, os retornos da api não estão segundo o combinado ainda e isso deve ser refatorado o quanto antes;
       navigate('/register');
