1b0314ad5a35ed7ce7f0126374da203ec87c8ab6
C.Swame <caio_swame@hotmail.com>
2022-08-24 23:21:03 -0300

    feat[frontEnd]: allows the user to login if registeres

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 // import axios from 'axios';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import api from '../services/request';
 import { changeEmail } from '../Redux/slicers/user.slicer';
 
 function Login() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  // const [hasAnError, setError] = useState('');
+  const [hasAnError, setError] = useState(false);
 
   const isButtonDisabled = () => {
     const PASSWORD_LENGTH = 6;
@@ -21,11 +23,19 @@ function Login() {
     );
   };
 
-  const onClickButton = (event) => {
+  const onClickButton = async (event) => {
     event.preventDefault();
-    /*     const loginData = axios.post('./localhost:3001', { email, password });
-    if (loginData.error)  */
-    dispatch(changeEmail(email));
+    try {
+      const loginData = await api.post('/login', { email, password });
+      if (loginData.error) return setError(true);
+      dispatch(changeEmail(email));
+      // *** salvar o token recebido;
+      // *** tratar o erro de forma diferente. Quando os requests tem retorno com status de erro o programa está crashando. Buscar uma opção do axios ou react que evite isso (se existir);
+      // *** Apesar do problema acima, os retornos da api não estão segundo o combinado ainda e isso deve ser refatorado o quanto antes;
+      navigate('/register');
+    } catch (err) {
+      console.log(err.message);
+    }
   };
 
   return (
@@ -50,9 +60,10 @@ function Login() {
           />
         </label>
 
-        <span className={ hasAnError }>
-          email inválido
-        </span>
+        {hasAnError && (
+          <span className={ hasAnError } style>
+            email ou senha inválidos
+          </span>)}
         <button
           type="submit"
           aria-label="login-button"
