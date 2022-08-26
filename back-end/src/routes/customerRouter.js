d8117c16ade556a3cf0638681d7db31ccfd5e315
6u7o <augustopinheiro19999999@gmail.com>
2022-08-26 18:51:56 -0300

    FEAT[back-end]: create router for checkout enpoint

---
+++ back-end/src/routes/customerRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { getAllProducts } = require('../controllers/productsController');
+const { createSale } = require('../controllers/checkoutController');
+
+const customerRouter = express.Router();
+
+customerRouter.get('/products', getAllProducts);
+customerRouter.get('/checkout', createSale);
+
+module.exports = customerRouter;
--- back-end/src/routes/productsRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getAllProducts } = require('../controllers/productsController');
-
-const productsRouter = express.Router();
-
-productsRouter.get('/products', getAllProducts);
-
-module.exports = productsRouter;
