ec453d86a9b48a465bd3f10dce31bccdfb69f746
André <amorimtorresandre@gmail.com>
2022-08-26 17:41:25 -0300

    FEAT: header component

--- front-end/src/Components/Header.jsx
+++ front-end/src/Components/Header.jsx
@@ -3,7 +3,33 @@ import React from 'react';
 function Header() {
   return (
     <div>
-      <h1> Header </h1>
+      <button
+        type="button"
+        aria-label="header-button1"
+        name="header-button1"
+        // onClick={}
+      >
+        LALALAND
+      </button>
+      <button
+        type="button"
+        aria-label="header-button2"
+        name="header-button2"
+        // onClick={}
+      >
+        ARTURITO
+      </button>
+      <div>
+        <h3> PESSOA USUÁRIA </h3>
+      </div>
+      <button
+        type="button"
+        aria-label="checkout-button"
+        name="checkout-button"
+        // onClick={}
+      >
+        SAIR
+      </button>
     </div>
   );
 }
