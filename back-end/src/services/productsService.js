f7ea5dab0a1cc2f9a7a0bfdf625756e046068feb
c.swame <93006927+c-swame@users.noreply.github.com>
2022-08-26 18:59:11 -0300

    Lint[backend service]
    lint adjust

--- back-end/src/services/productsService.js
+++ back-end/src/services/productsService.js
@@ -1,7 +1,7 @@
 const { Products } = require('../database/models');
 
-const getAllProducts = async (email, password) => {
-  const arrProducts = await Products.findAll({raw: true});
+const getAllProducts = async () => {
+  const arrProducts = await Products.findAll({ raw: true });
   console.log(arrProducts);
   if (!arrProducts) return false;
 
