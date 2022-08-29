59c6fa6deb978c5b54076223902413aa398228d8
C.Swame <caio_swame@hotmail.com>
2022-08-29 17:22:07 -0300

    fix[backend controller checkoutController]: update function to get the requester userId from the token

--- back-end/src/controllers/checkoutController.js
+++ back-end/src/controllers/checkoutController.js
@@ -1,14 +1,13 @@
 const checkoutService = require('../services/checkoutService');
 
 const createSale = async (req, res) => {
-  console.log('Chegou');
   const { saleData, products } = req.body;
-  const response = await checkoutService.createSale({ ...saleData, userId: 3, products });
+  const response = await checkoutService.createSale({ ...saleData, userId: req.UserId, products });
   // if (!response) {
   //   throw Error('Not found');
   // } 
   // return res.status(201).json({data: response});
-  return res.status(201).json(response);
+  return res.status(201).json({ data: response });
 };
 
 module.exports = {
