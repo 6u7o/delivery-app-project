907c1f011be2a4f706a9c829f3b3845e038cc211
C.Swame <caio_swame@hotmail.com>
2022-08-29 17:17:34 -0300

    feat[backend route validation]: add a token validation middleware to the endpoint 'post /customer/checkout'

--- back-end/src/routes/customerRouter.js
+++ back-end/src/routes/customerRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { getAllProducts } = require('../controllers/productsController');
 const { createSale } = require('../controllers/checkoutController');
+const jwtValidateAndRefreshToken = require('../middlewares/jwtValidateAndRefresh.middlewar');
 
 const customerRouter = express.Router();
 
 customerRouter.get('/products', getAllProducts);
-customerRouter.post('/checkout', createSale);
+customerRouter.post('/checkout', jwtValidateAndRefreshToken, createSale);
 // customerRouter.post('/orders/:id', createSale);
 
 module.exports = customerRouter;
