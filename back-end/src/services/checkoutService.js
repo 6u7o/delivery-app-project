346ae00c2bc41765238d6f6e811593226887e553
C.Swame <caio_swame@hotmail.com>
2022-08-29 17:00:12 -0300

    chore[backend services checkoutService.js]: implements a second query to update the table salesProducts

--- back-end/src/services/checkoutService.js
+++ back-end/src/services/checkoutService.js
@@ -1,14 +1,26 @@
-const { Sales, sequelize } = require('../database/models');
+const { Sales, SalesProducts, sequelize } = require('../database/models');
 // const Sequelize = require('../database/models');
 
 const createSale = async (saleInfo) => {
   const transaction = await sequelize.transaction();
-
   try {
-    const newSale = await Sales.create(saleInfo, { raw: true, transaction });
-    console.log('Chegou aqui', newSale.dataValues);  
+    const newSale = await Sales.create(saleInfo, {
+      raw: true,
+      transaction,
+      fields: ['sellerId', 'totalPrice', 'deliveryAddress', 'deliveryNumber', 'userId']
+    });
+    console.log('Chegou aqui', newSale.dataValues);
+
+    const formatedProducts = saleInfo.products.map(({ productId, quantity }) => {
+      return { saleId: newSale.id, productId, quantity }
+    })
+
+    await SalesProducts.bulkCreate(formatedProducts, { transaction });
+    
+    await transaction.commit();
     return newSale.dataValues;
   } catch (error) {
+    console.log('catch: ***', error.message)
     await transaction.rollback();
   }
   
