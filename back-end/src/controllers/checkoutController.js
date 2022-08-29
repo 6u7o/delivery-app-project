f7894d2b617dae120fda7a9c91e45de4bb9c9cc1
C.Swame <caio_swame@hotmail.com>
2022-08-29 16:58:38 -0300

    refac[backend controllers checkoutContreller]: change service argument construction

--- back-end/src/controllers/checkoutController.js
+++ back-end/src/controllers/checkoutController.js
@@ -2,7 +2,8 @@ const checkoutService = require('../services/checkoutService');
 
 const createSale = async (req, res) => {
   console.log('Chegou');
-  const response = await checkoutService.createSale(req.body);
+  const { saleData, products } = req.body;
+  const response = await checkoutService.createSale({ ...saleData, userId: 3, products });
   // if (!response) {
   //   throw Error('Not found');
   // } 
