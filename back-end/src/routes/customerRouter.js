b9bb1c5c261a9c91493c4e21a30f1fa4f9b1a97c
C.Swame <caio_swame@hotmail.com>
2022-08-29 17:54:20 -0300

    feat[route 'post /customers/sales' validation]

--- back-end/src/routes/customerRouter.js
+++ back-end/src/routes/customerRouter.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { getAllProducts } = require('../controllers/productsController');
 const { createSale } = require('../controllers/checkoutController');
 const jwtValidateAndRefreshToken = require('../middlewares/jwtValidateAndRefresh.middlewar');
+const postNewSaleMiddleware = require('../middlewares/postNewSaleMiddleware.middlewares');
 
 const customerRouter = express.Router();
 
 customerRouter.get('/products', getAllProducts);
-customerRouter.post('/checkout', jwtValidateAndRefreshToken, createSale);
+customerRouter.post('/checkout', jwtValidateAndRefreshToken, postNewSaleMiddleware, createSale);
 // customerRouter.post('/orders/:id', createSale);
 
 module.exports = customerRouter;
