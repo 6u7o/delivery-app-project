c9a03621fb92f6d331ec74091351c1ef11453027
C.Swame <caio_swame@hotmail.com>
2022-08-30 11:44:48 -0300

    feat[backend routes]: creates a route 'get /customer/orders' that revices a token and returns the user orders

--- back-end/src/routes/customerRouter.js
+++ back-end/src/routes/customerRouter.js
@@ -3,11 +3,14 @@ const { getAllProducts } = require('../controllers/productsController');
 const { createSale } = require('../controllers/checkoutController');
 const jwtValidateAndRefreshToken = require('../middlewares/jwtValidateAndRefresh.middlewar');
 const postNewSaleMiddleware = require('../middlewares/postNewSaleMiddleware.middlewares');
+const costumerController = require('../controllers/customerController');
 
 const customerRouter = express.Router();
 
 customerRouter.get('/products', getAllProducts);
 customerRouter.post('/checkout', jwtValidateAndRefreshToken, postNewSaleMiddleware, createSale);
-// customerRouter.post('/orders/:id', createSale);
+customerRouter.get('/orders/',
+  jwtValidateAndRefreshToken,
+  costumerController.getAllOrdersByUserId);
 
 module.exports = customerRouter;
