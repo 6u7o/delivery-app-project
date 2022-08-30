e42e4b0464ba688fb7bfe2602d25c716b1dd292f
C.Swame <caio_swame@hotmail.com>
2022-08-30 13:33:36 -0300

    chore[backend controller customerController getOrderProducts]: creates a method to get products of an order

--- back-end/src/controllers/customerController.js
+++ back-end/src/controllers/customerController.js
@@ -8,6 +8,16 @@ const getAllOrdersByUserId = async (req, res) => {
   return res.status(200).json({ data: response, token: req.NewToken });
 };
 
+const getOrderProducts = async (req, res) => {
+  const { orderId } = req.params;
+  const response = await userServices.getOrderProducts(req.UserId, orderId);
+  if (!response) {
+    throw Error('Not found');
+  } 
+  return res.status(200).json({ data: response, token: req.NewToken });
+};
+
 module.exports = {
   getAllOrdersByUserId,
+  getOrderProducts
 };
