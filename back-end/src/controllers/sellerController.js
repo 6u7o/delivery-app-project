91f1c31085ac4ff0f14305aa210b3304ee92be05
6u7o <augustopinheiro19999999@gmail.com>
2022-08-30 17:27:20 -0300

    FEAT[backEnd]: create enpoint to update order status

---
+++ back-end/src/controllers/sellerController.js
@@ -0,0 +1,16 @@
+const sellerService = require('../services/sellerService');
+
+const updateOrderStatus = async (req, res) => {
+  const { newStatus } = req.body;
+  const { orderId } = req.params;
+  // da pra fazer uma condição aqui para vetar algumas alteração de status que não fazem sentido
+  const response = await sellerService.updateOrderStatus(orderId, newStatus);
+  if (!response) {
+    throw Error('Not found');
+  } 
+  return res.status(201).json({ data: response });
+};
+
+module.exports = {
+  updateOrderStatus,
+};
