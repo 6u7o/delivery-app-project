a5f13afd3578e58ea15e8d2745fe6825fb358fa2
artur <artur.sds@hotmail.com>
2022-09-02 16:18:11 -0300

    fix [backEnd]: lint error

--- back-end/src/middlewares/errors/errorMiddleware.middlewares.js
+++ back-end/src/middlewares/errors/errorMiddleware.middlewares.js
@@ -1,7 +1,10 @@
 const errorMiddleware = (error, _req, res, _next) => {
   console.log(error.message);
-  if (error.original?.code === 'ER_DUP_ENTRY') res.status(409).json({ message: error.message, erro: true });
-
+  if (error.original && (error.original.code === 'ER_DUP_ENTRY')) {
+    // !!!!!!!!!!!!!!!!!!! 
+    res.status(409).json({ message: error.message, erro: true });
+    // !!!!!!!!!!!!!!!!!!!
+  }
   return res.status(500).json({ message: error.message, erro: 'teste' });
 };
 
