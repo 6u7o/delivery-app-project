b751a590f3ed9c9c13d9b841710558442866b6c2
artur <artur.sds@hotmail.com>
2022-09-02 15:59:38 -0300

    fix [backEnd]: erros ao encriptar senha no cadastro de usuarios

--- back-end/src/joi/createNewUser.joi.js
+++ back-end/src/joi/createNewUser.joi.js
@@ -1,14 +1,14 @@
 const joi = require('joi');
 
 const newUserDataSchema = joi.object({
-  name: joi.string().min(3).required(),
+  name: joi.string().min(12).required(),
   email: joi.string().email().required()
     .messages({
       'string.email': 'invalid email///422',
     }),
-  password: joi.string().min(8).required()
+  password: joi.string().min(6).required()
     .messages({
-      'string.min': '{{#label}} length must be at least 8 characters long///422' }),
+      'string.min': '{{#label}} length must be at least 6 characters long///422' }),
 }).messages({
   'any.required': '{{#label}} is required///400',
   'string.min': '{{#label}} length must be at least 3 characters long///422', 
