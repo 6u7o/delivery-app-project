923f80775a02dc918a5b18323b35157fe68235a0
André <amorimtorresandre@gmail.com>
2022-09-08 18:38:39 -0300

    style: styled components preset

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import defaultTheme from './Assets/Styles/themes/default';
+import GlobalStyles from './Assets/Styles/global';
 import './App.css';
 
-import Login from './Pages/Login';
+import Login from './Pages/Login/Login';
 import Register from './Pages/Register';
 import Checkout from './Pages/Checkout';
 import Manage from './Pages/Manage';
@@ -15,18 +18,21 @@ import Products from './Pages/Products';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={ <Login /> } />
-        <Route exact path="/" element={ <Navigate to="/login" /> } />
-        <Route path="/register" element={ <Register /> } />
-        <Route path="/customer/products" element={ <Products /> } />
-        <Route path="/customer/orders" element={ <CustomerOrders /> } />
-        <Route path="/customer/orders/:id" element={ <CustomerOrdersDetails /> } />
-        <Route path="/customer/checkout" element={ <Checkout /> } />
-        <Route path="/seller/orders" element={ <SellerOrders /> } />
-        <Route path="/seller/orders/:id" element={ <SellerOrdersDetails /> } />
-        <Route path="/admin/manage" element={ <Manage /> } />
-      </Routes>
+      <ThemeProvider theme={ defaultTheme }>
+        <Routes>
+          <Route path="/login" element={ <Login /> } />
+          <Route exact path="/" element={ <Navigate to="/login" /> } />
+          <Route path="/register" element={ <Register /> } />
+          <Route path="/customer/products" element={ <Products /> } />
+          <Route path="/customer/orders" element={ <CustomerOrders /> } />
+          <Route path="/customer/orders/:id" element={ <CustomerOrdersDetails /> } />
+          <Route path="/customer/checkout" element={ <Checkout /> } />
+          <Route path="/seller/orders" element={ <SellerOrders /> } />
+          <Route path="/seller/orders/:id" element={ <SellerOrdersDetails /> } />
+          <Route path="/admin/manage" element={ <Manage /> } />
+        </Routes>
+        <GlobalStyles />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
